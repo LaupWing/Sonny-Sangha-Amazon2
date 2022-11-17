c09f9c0c811f8910864a497d2b7368fd34ce2ceb
LaupWing <loc_strict@hotmail.com>
2022-11-17 18:58:22 +0100

    update

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+   apiKey: "AIzaSyCpnF3zeFI9v5dNOtLHDlbuhovfo7__M-Q",
+   authDomain: "sonny-sangha.firebaseapp.com",
+   projectId: "sonny-sangha",
+   storageBucket: "sonny-sangha.appspot.com",
+   messagingSenderId: "789737271920",
+   appId: "1:789737271920:web:079cc02f568f8faacc7ab5",
+   measurementId: "G-0T7ETG8F46",
+}
