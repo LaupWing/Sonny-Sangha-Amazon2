ba593ccb80ea17c0a6896d0fc4dee845e6f64786
LaupWing <loc_strict@hotmail.com>
2022-11-18 17:31:42 +0100

    update

---
+++ src/pages/checkout.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/Header"
+
+const CheckoutPage = () => {
+   return (
+      <div className="bg-gray-100">
+         <Header />
+         <main className="lg:flex max-w-screen-2xl mx-auto">Hello</main>
+      </div>
+   )
+}
+
+export default CheckoutPage
