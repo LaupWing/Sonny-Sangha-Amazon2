dd9b4213a0ecaeb7458cb8bb6bc272b85a9ed6c4
LaupWing <loc_strict@hotmail.com>
2022-11-22 13:08:58 +0100

    udpate

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -13,6 +13,25 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 const endpointSecret = process.env.STRIPE_SIGNING_SECRET
 
+const fulfillOrder = async (session) => {
+   console.log("Fulfilling order", session)
+
+   return app
+      .firestore()
+      .collection("users")
+      .doc(session.metadata.email)
+      .collection("orders")
+      .doc(session.id)
+      .set({
+         amount: session.amount_total / 100,
+         images: JSON.parse(session.metadata.images),
+         timestamp: admin.firestore.FieldValue.serverTimestamp(),
+      })
+      .then(() => {
+         console.log(`Success: order ${session.id} has been added to the DB`)
+      })
+}
+
 export default async (req, res) => {
    if (req.method === "POST") {
       const requestBuffer = await buffer(req)
@@ -26,5 +45,22 @@ export default async (req, res) => {
          console.error(e.message)
          return res.status(400).send(`Webhook error ${err.message}`)
       }
+
+      if (event.type === "checkout.session.completed") {
+         const session = event.data.object
+
+         return fulfillOrder(session)
+            .then(() => res.status(200))
+            .catch((err) =>
+               res.status(400).send(`Webhook error ${err.message}`)
+            )
+      }
    }
+}
+
+export const config = {
+   api: {
+      bodyParser: false,
+      externalResolver: true,
+   },
 }
