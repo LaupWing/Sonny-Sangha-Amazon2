c2493f0a46fbe6f984575a4c4bd447b695710b22
LaupWing <loc_strict@hotmail.com>
2022-11-22 12:58:33 +0100

    udpate

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,9 +1,30 @@
 import { buffer } from "micro"
 import * as admin from "firebase-admin"
 
-// let
+const serviceAccount = require("../../../permissions.json")
+
+const app = !admin.apss.length
+   ? admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+     })
+   : admin.app()
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET
 
 export default async (req, res) => {
    if (req.method === "POST") {
+      const requestBuffer = await buffer(req)
+      const payload = requestBuffer.toString()
+      const sig = req.headers["stripe-signature"]
+
+      let event
+      try {
+         event = stripe.webhooks.constructEvent(payload, sig, endpointSecret)
+      } catch (e) {
+         console.error(e.message)
+         return res.status(400).send(`Webhook error ${err.message}`)
+      }
    }
 }
