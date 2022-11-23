c50d865bb39cc4b9f06bac30158a8ddcdf089a75
LaupWing <loc_strict@hotmail.com>
2022-11-23 12:49:51 +0100

    update

--- firebase.js
+++ firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase"
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
    apiKey: "AIzaSyCpnF3zeFI9v5dNOtLHDlbuhovfo7__M-Q",
@@ -8,3 +10,9 @@ const firebaseConfig = {
    appId: "1:789737271920:web:079cc02f568f8faacc7ab5",
    measurementId: "G-0T7ETG8F46",
 }
+
+const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
+
+const db = app.firestore()
+
+export default db
